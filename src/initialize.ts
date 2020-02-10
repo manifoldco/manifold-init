5ae38bead0689c7212172a57cc855f421e4b0c8f
David Leger <dleger@manifold.co>
2020-02-10 17:29:24 -0400

    dynamically import older connections

--- src/initialize.ts
+++ src/initialize.ts
@@ -1,27 +1,29 @@
-import connection_0, { InitDetail as InitDetail_0 } from './v0';
+import connection, { InitDetail as InitDetail_0 } from './v0';
 
 type InitDetail = InitDetail_0;
 
-const getConnection = (e: CustomEvent<InitDetail>) => {
+const LATEST = 0;
+
+const getConnection = async (e: CustomEvent<InitDetail>) => {
   const { version } = e.detail;
+  // Pre-load latest version
+  if (version === LATEST) {
+    return connection(e);
+  }
 
-  const connections = {
-    0: connection_0(e),
-  };
-
-  const currentVersion = connections[version];
-
-  if (!currentVersion) {
+  // Lazy-import older versions
+  try {
+    return (await import(`./v${version}`)).default(e);
+  } catch {
     throw new Error(
       `Version ${version} doesn't exist. Ensure you have the latest release of mui-core.`
     );
   }
-  return currentVersion;
 };
 
-const onInitialize = (e: CustomEvent<InitDetail>) => {
+const onInitialize = async (e: CustomEvent<InitDetail>) => {
   try {
-    e.detail.resolve(getConnection(e));
+    e.detail.resolve(await getConnection(e));
   } catch (error) {
     e.detail.reject(error);
   }
