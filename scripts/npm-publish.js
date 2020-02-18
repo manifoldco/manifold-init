97bff0ed38788818b25407487b04e68aacb2d83f
David Leger <dleger@manifold.co>
2020-02-18 13:23:43 -0400

    parse tag name

--- scripts/npm-publish.js
+++ scripts/npm-publish.js
@@ -1,4 +1,7 @@
-console.log(`I'm an action!`, process.env.TAG);
+const { readFileSync, writeFileSync } = require('fs');
+const { resolve } = require('path');
+
+const SELECT_TAG = /-.*/; // Selects anything after a hyphen, or nothing
 
 /**
  * This ensures npm deploys get tagged correctly from Git:
@@ -6,6 +9,18 @@ console.log(`I'm an action!`, process.env.TAG);
  * v1.2.3-alpha.0 -> tag “alpha”
  * v1.2.3-rc.0    -> tag “rc”
  */
+const parseTagName = version => {
+  const tag = SELECT_TAG.exec(version);
+
+  if (tag && tag[0].length > 1) {
+    // Remove hyphens and anything trailing a dot
+    const tagName = tag.replace('-', '').replace(/\.[^.]+$/, '');
+    return tagName;
+  }
+
+  // default
+  return 'latest';
+};
 
 /* eslint-disable no-console */
 const version = process.env.TAG;
@@ -13,8 +28,20 @@ const timeStart = process.hrtime();
 
 console.log(`📦 Publishing ${version} to npm...`);
 
+if (version) {
+  const pkgJSON = resolve(__dirname, '..', 'package.json');
+  const pkgManifest = JSON.parse(readFileSync(pkgJSON, 'utf8'));
+  pkgManifest.version = version;
+  writeFileSync(pkgJSON, JSON.stringify(pkgManifest, null, 2).concat('\n'), 'utf8');
+}
+
+// TODO make more readable
+// 2. Determine if @latest or @[other]
+const npmTag = parseTagName(version);
+console.log({ npmTag });
+
 // 3. Publish to npm
-// execSync(`npm publish ../pkg --tag ${npmTag}`, {
+// execSync(`npm publish --tag ${npmTag}`, {
 //   cwd: __dirname, // run the command from this folder (so it can run anywhere)
 // });
 
