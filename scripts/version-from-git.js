e931e3a96f16b2919d83d9c4622316d81d409a1f
David Leger <dleger@manifold.co>
2020-02-21 13:05:44 -0400

    publish types package

--- scripts/version-from-git.js
+++ scripts/version-from-git.js
@@ -18,7 +18,14 @@ try {
 }
 
 if (version) {
+  // main package
   const pkgJSON = path.resolve(__dirname, '..', 'package.json');
+  const pkgManifest = JSON.parse(fs.readFileSync(pkgJSON, 'utf8'));
+  pkgManifest.version = version;
+  fs.writeFileSync(pkgJSON, JSON.stringify(pkgManifest, null, 2).concat('\n'), 'utf8');
+
+  // types package
+  const pkgJSON = path.resolve(__dirname, '..', 'mui-core-types', 'package.json');
   const pkgManifest = JSON.parse(fs.readFileSync(pkgJSON, 'utf8'));
   pkgManifest.version = version;
   fs.writeFileSync(pkgJSON, JSON.stringify(pkgManifest, null, 2).concat('\n'), 'utf8');
