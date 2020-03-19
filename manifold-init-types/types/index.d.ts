aec572ad15a4340a574762aec868eed971ef09ee
Sam Slotsky <sam@manifold.co>
2020-03-19 12:24:23 -0500

    Remove copied types

--- manifold-init-types/types/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { initialize } from './core';
-export * from './components';
--- manifold-init-types/types/v0/gateway.d.ts
+++
@@ -1,7 +0,0 @@
-export interface Gateway {
-    post: <Resp, Req extends {}>(path: string, body: Req) => Promise<Resp>;
-}
-export declare function createGateway({ baseUrl, retries, }: {
-    baseUrl?: () => string;
-    retries?: number;
-}): Gateway;
